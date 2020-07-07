ac4e5c571a379a5003795cff1d74caf7f22dd98a
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-07 13:17:21 -0400

    setup imports

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
+import re 
+from string import letters
 
 import jinja2
 import webapp2
+
+from google.appengine.ext import db 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
