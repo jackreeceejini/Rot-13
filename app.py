ade67aa9e40df895e5f166be848655b7352b9326
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-07 13:24:27 -0400

    added username, email and password validation

--- app.py
+++ app.py
@@ -32,5 +32,18 @@ class Rot13(BaseHandler):
             rot13 = text.encode('rot13')
 
         self.render('rot13-form.html', text = rot13)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
         
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
